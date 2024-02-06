38d22fc47bb90becf2b51ea5aa7e68be5c74a434
AlDeGWiN <ivanliagov327@gmail.com>
2024-02-07 00:47:06 +0300

    Initialize project

---
+++ src/test/java/com/aldegwin/budgetplanner/BudgetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldegwin.budgetplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
